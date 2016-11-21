556fbbb6ec7cff4deddc52553dffccc3b29d03af
Saravana <saran@agiratech.com>
2016-11-21 18:04:01 +0530

    Feeds listing method added

--- app/controllers/news_feeds_controller.rb
+++ app/controllers/news_feeds_controller.rb
@@ -1,10 +1,24 @@
 class NewsFeedsController < ApplicationController
 
+  before_action :set_user
   # Action to perform news feeds listing
   def index
-    user = User.find(params[:user_id])
-    feeds = user.user_feeds # Will return feeds for given user
-    render :json => {"status" => 0, "feeds" => feeds}.to_json
+    begin
+      feeds = @user.user_feeds(params[:page, params[:limit]]) # Will return feeds for given user
+      render :json => {"status" => 1, "feeds" => feeds}.to_json
+    rescue
+      render :json => {"status" => 0, "errors" => "Something went wrong"}.to_json
+    end
+  end
+
+  private
+
+  # To set user
+  def set_user
+    @user = User.where("id =?",params[:user_id]).first
+    if @user.nil?
+      render :json => {"status" => 0, "errors" => "User not found"}.to_json
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_many :followers
   has_many :posts
   has_many :feeds
+
+  def user_feeds page, limit
+    self.feeds.order("created_at DESC")
+  end
 end
