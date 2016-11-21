bfb45d30d253464cb2d54adf1fa63dee9284ac25
Saravana <saran@agiratech.com>
2016-11-21 17:51:07 +0530

    Feeds controller added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,21 +1,6 @@
-module V2
-  require "#{Rails.root}/lib/jurni_event"
+class WelcomeController < ApplicationController
 
-  class WelcomeController < ApplicationController
-    def show
-      render :plain => "Hello World"
-    end
-
-    def register
-      begin
-        # V2 app secret
-        raise "Invalid app_secret" unless params["app_secret"] == "8YHsvw7fuylbLr5FevrFAsRC/v2sH5X8i9aWODH76908GxhIE/+jDj0cVJft+zTx2WkQmxiGM06KAnBtG1C7gg=="
-        ak = V2::APIKey.register(params)
-        JurniEvent.new_api_key_event(ak)
-        render :json => {"status" => 1, "api_key" => ak.api_key.to_s, "api_secret" => ak.api_secret.to_s}
-      rescue => e
-        render :json => {"status" => 0, "error" => e.to_s}.to_json
-      end
-    end
+  def index
+    render :plain => "Hey! API endpoint is up and running"
   end
 end
