92487ceb832c8370c4025ecd1dfd8f547aca708d
Saravana <saran@agiratech.com>
2016-11-21 17:31:09 +0530

    Gems added for delayed jobs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :check_rack_mini_profiler
+
+  def check_rack_mini_profiler
+    # for example - if current_user.admin?
+    if params[:rmp]
+      Rack::MiniProfiler.authorize_request
+    end
+  end
 end
