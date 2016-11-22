c7c82249dd7fca0b047bacf42038ffeeb9bc5b02
Saravana <saran@agiratech.com>
2016-11-22 13:15:17 +0530

    rspec code modified for uer profile picture

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :user do
     user_name 'AnyName'
     geo_location 'AnyLocation'
-    profile_picture 'AnyPicture'
-  
+
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,22 +8,17 @@ RSpec.describe User, :type => :model do
   end
 
   it "user doesn't valid with invalid params" do
-    user = User.create(user_name: nil, geo_location: nil, profile_picture: nil)
+    user = User.create(user_name: nil, geo_location: nil)
     expect(user).to_not be_valid
   end
 
   it "user doesn't valid with invalid user_name" do
-    user = User.create(user_name: nil, geo_location: 'AnyLocation', profile_picture: 'AnyPicture')
+    user = User.create(user_name: nil, geo_location: 'AnyLocation')
     expect(user).to_not be_valid
   end
 
   it "user doesn't valid with invalid geo_location" do
-    user = User.create(user_name: 'AnyName', geo_location: nil, profile_picture: 'AnyPicture')
-    expect(user).to_not be_valid
-  end
-
-   it "user doesn't valid with invalid profile_picture" do
-    user = User.create(user_name: 'AnyName', geo_location: 'AnyLocation', profile_picture: nil)
+    user = User.create(user_name: 'AnyName', geo_location: nil)
     expect(user).to_not be_valid
   end
 
