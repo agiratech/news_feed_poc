674d4096c99919c329b9aaa9e589d82186224662
Saravana <saran@agiratech.com>
2016-11-22 11:10:52 +0530

    Feeds added

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'memory_profiler'
 gem 'delayed_job_active_record'
 gem 'activerecord-import'
 gem "daemons"
+gem 'will_paginate'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # API endpoint check
   root 'welcome#index'
 
-  get 'feeds' => 'news_feeds#index'
+  get 'feeds' => 'news_feeds#feeds'
+  get 'activities' => 'news_feeds#feeds'
+  get 'feeds' => 'news_feeds#feeds'
 
 end
